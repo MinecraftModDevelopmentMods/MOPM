2a36bc8a2e7c8d28da38f1bf79a730bececdb80d
TheUnknownFew <zedlander1000@yahoo.com>
2020-05-08 17:49:06 -0400

    Environment has been updated to 1.15.2

--- src/main/java/zed/mopm/ModRegistry.java
+++ src/main/java/zed/mopm/ModRegistry.java
@@ -3,40 +3,22 @@ package zed.mopm;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLFingerprintViolationEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import zed.mopm.gui.events.EventMenuOpened;
 import zed.mopm.util.References;
 
-@Mod(
-        modid = References.MODID,
-        name = References.NAME,
-        version = References.VERSION,
-        certificateFingerprint = "@FINGERPRINT@",
-        clientSideOnly = true
-)
+@Mod("MOPM")
 public class ModRegistry {
 
-    //temp
-    @Mod.EventHandler
-    public void preModInit(final FMLPreInitializationEvent preInit) {
-        // NOTHING TO SEE HERE
-    }
+	public ModRegistry() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::startClient);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
 
-    @Mod.EventHandler
-    public void modInit(final FMLInitializationEvent init) {
-        MinecraftForge.EVENT_BUS.register(EventMenuOpened.class);
-    }
-
-    @Mod.EventHandler
-    public void modLoader(final FMLPostInitializationEvent postInit) {
-        EventMenuOpened.loadMenus(Minecraft.getMinecraft().currentScreen);
-    }
-
-    @Mod.EventHandler
-    public void onFingerprintViolation(final FMLFingerprintViolationEvent event) {
-        References.LOG.warn(References.INVALID_FINGERPRINT);
-    }
+	private void startClient(final FMLClientSetupEvent event) {
+		MinecraftForge.EVENT_BUS.register(EventMenuOpened.class);
+		EventMenuOpened.loadMenus(Minecraft.getInstance().currentScreen);
+	}
 }
